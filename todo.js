dde78af0be12d2748b050449336aa0ca7141a271
TAK <infernal-slam@hotmail.com>
2016-03-21 01:26:45 +0700

    first

--- todo.js
+++ todo.js
@@ -2,17 +2,42 @@
 angular.module('todoApp', [])
  .controller('TodoListController', function () {
    var todoList = this
-   todoList.title = 'testef'
-   todoList.data = []
-   var checklist = []
-   todoList.add = function (book, price, index) {
-     console.log(book + '/' + price)
-     var data = {
-       namebook: book,
-       price: price
+   todoList.products = [
+     {
+       id: 1,
+       pic: 'img/hp_1.jpg',
+       name: 'harry potter ศิลาอาถรรพ',
+       price: 100.00
+     }, {
+       id: 2,
+       pic: 'img/hp_2.jpg',
+       name: 'harry potter ห้องแห่งความลับ',
+       price: 100.00
+     }, {
+       id: 3,
+       pic: 'img/hp_3.jpg',
+       name: 'harry potter นักโทษแห่งอัซคาบัน',
+       price: 100.00
+     }, {
+       id: 4,
+       pic: 'img/hp_4.jpg',
+       name: 'harry potter ถ้วยอัคนี',
+       price: 100.00
+     }, {
+       id: 5,
+       pic: 'img/hp_5.jpg',
+       name: 'harry potter ภาคีนกฟีนิกซ์',
+       price: 100.00
+     }, {
+       id: 6,
+       pic: 'img/hp_6.jpg',
+       name: 'harry potter เจ้าชายเลือดผสม',
+       price: 100.00
+     }, {
+       id: 7,
+       pic: 'img/hp_7.jpg',
+       name: 'harry potter เครื่องรางยมทูต',
+       price: 100.00
      }
-     todoList.data.push(data)
-     checklist.push(index)
-     // console.log(checklist)
-   }
+   ]
  })
