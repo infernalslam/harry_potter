298a12fc60ff51e74631180bc2a372120c1afd67
TAK <infernal-slam@hotmail.com>
2016-03-20 21:28:45 +0700

    failv1

--- todo.js
+++ todo.js
@@ -3,13 +3,16 @@ angular.module('todoApp', [])
  .controller('TodoListController', function () {
    var todoList = this
    todoList.title = 'testef'
-   todoList.data = [{}]
-   todoList.add = function (book, price) {
+   todoList.data = []
+   var checklist = []
+   todoList.add = function (book, price, index) {
      console.log(book + '/' + price)
      var data = {
        namebook: book,
        price: price
      }
      todoList.data.push(data)
+     checklist.push(index)
+     // console.log(checklist)
    }
  })
