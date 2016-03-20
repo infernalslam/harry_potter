343e3f2a5c91179899cc15646bb30171c4a64c9b
TAK <infernal-slam@hotmail.com>
2016-03-20 18:44:43 +0700

    frist

--- todo.js
+++ todo.js
@@ -3,4 +3,13 @@ angular.module('todoApp', [])
  .controller('TodoListController', function () {
    var todoList = this
    todoList.title = 'testef'
+   todoList.data = [{}]
+   todoList.add = function (book, price) {
+     console.log(book + '/' + price)
+     var data = {
+       namebook: book,
+       price: price
+     }
+     todoList.data.push(data)
+   }
  })
