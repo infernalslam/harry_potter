11a436b5fdb4f04d0ec86a49211d161185034c12
TAK <infernal-slam@hotmail.com>
2016-03-23 02:01:40 +0700

    test

--- todo.js
+++ todo.js
@@ -3,6 +3,7 @@ angular.module('todoApp', [])
   .controller('TodoListController', function () {
     var todoList = this
     todoList.show = []
+    todoList.totalamount = []
     todoList.add = function (id, name, price) {
       if (checkarr(todoList.show, id)) {
         var index = findarr(todoList.show, id)
@@ -16,7 +17,15 @@ angular.module('todoApp', [])
         }
         todoList.show.push(data_book)
       }
-      console.log(todoList.show)
+      todoList.show.sort(function (a, b) {
+        if (a.amount > b.amount) {
+          return -1
+        }
+        if (a.amount < b.amount) {
+          return 1
+        }
+        return 0
+      })
     }
     var checkarr = function (arr, id) {
       for (var i = 0; i < arr.length; i++) {
