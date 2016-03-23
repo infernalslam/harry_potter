687ade2d7694b11a772b806871ca7a1ffde394a2
infernalslam <infernal-slam@hotmail.com>
2016-03-24 02:42:36 +0700

    fuck up

--- todo.js
+++ todo.js
@@ -2,90 +2,88 @@
 angular.module('todoApp', [])
   .controller('TodoListController', function () {
     var todoList = this
-    todoList.show = []
-    todoList.totalamount = []
-    todoList.promotion = []
-    todoList.add = function (id, name, price) {
-      if (checkarr(todoList.show, id)) {
-        var index = findarr(todoList.show, id)
-        todoList.show[index].amount += 1
+    todoList.title = 'book store'
+    todoList.store = []
+    todoList.promo = []
+    todoList.discount = 0
+    todoList.total = 0
+    todoList.addBook = function (ep, name, price) {
+      if (checkStore(todoList.store, ep)) {
+        var index = addAmount(todoList.store, ep)
+        todoList.store[index].amount += 1
       } else {
-        var data_book = {
-          id: id,
+        var dataBook = {
+          ep: ep,
           name: name,
           price: price,
           amount: 1
         }
-        todoList.show.push(data_book)
+        todoList.store.push(dataBook)
       }
-      todoList.show.sort(function (a, b) {
-        if (a.amount > b.amount) {
-          return -1
-        }
-        if (a.amount < b.amount) {
-          return 1
-        }
+      todoList.store.sort(function (a, b) {
+        if (a.amount > b.amount) return -1
+        if (a.amount < b.amount) return 1
         return 0
       })
-      var amountproduct = []
-      for (var i = 0; i < todoList.show.length; i++) {
-        if (typeof todoList.show[i].amount !== 'undefined') {
-          amountproduct.push(todoList.show[i].amount)
+      var amount = []
+      for (var i = 0; i < todoList.store.length; i++) {
+        if (typeof todoList.store[i].amount !== 'undefined') {
+          amount.push(todoList.store[i].amount)
         }
       }
-      todoList.promotion = amountproduct
-      console.log(todoList.promotion)
+      todoList.promo = amount
+      console.log('todoList.promo :' + todoList.promo)
+    }
+    todoList.bill = function () {
+      var total = 0
+      for (var i = 0; i < todoList.store.length; i++) {
+        total += todoList.store[i].amount
+      }
+      return total
     }
     todoList.cal = function () {
-      var count_book = 0
-      // var max = todoList.promotion[0]
-      var discount = 0
-      for (var i = 0; i <= todoList.promotion.length; i++) {
-        for (var j = 0; j < todoList.promotion.length; j++) {
-          if (todoList.promotion[j] > 0) {
-            count_book++
+      var countbook = 0
+      todoList.discount = 0
+      var max = 0
+      for (var i = 0; i <= todoList.promo.length; i++) {
+        for (var j = 0; j < todoList.promo.length; j++) {
+          if (todoList.promo[j] > 0) {
+            countbook++
+            if (max < countbook) {
+              max = countbook
+            }
           }
-          console.log('เหลือกี่คู่ :' + count_book)
         }
-        if (count_book === 2) {
-          discount += 20
-          console.log('20')
-        } else if (count_book === 3) {
-          discount += 60
-          console.log('60')
-        } else if (count_book === 4) {
-          discount += 120
-          console.log('120')
-        } else if (count_book === 5) {
-          discount += 200
-          console.log('200')
-        } else if (count_book === 6) {
-          discount += 300
-          console.log('300')
-        } else if (count_book === 7) {
-          discount += 420
-          console.log('420')
+        if (countbook === 2) {
+          todoList.discount += 20
+        } else if (countbook === 3) {
+          todoList.discount += 60
+        } else if (countbook === 4) {
+          todoList.discount += 120
+        } else if (countbook === 5) {
+          todoList.discount += 200
+        } else if (countbook === 6) {
+          todoList.discount += 300
         }
-        for (var k = 0; k < todoList.promotion.length; k++) {
-          todoList.promotion[k] -= 1
+        for (var k = 0; k < todoList.promo.length; k++) {
+          todoList.promo[k] -= 1
         }
-        count_book = 0
+        countbook = 0
       }
-      console.log('ส่วนลด :' + discount)
+      console.log('todoList.discount' + todoList.discount)
     }
-    var checkarr = function (arr, id) {
-      for (var i = 0; i < arr.length; i++) {
-        if (arr[i].id === id) {
+    var checkStore = function (store, ep) {
+      for (var i = 0; i < store.length; i++) {
+        if (store[i].ep === ep) {
           return true
         }
       }
     }
-    var findarr = function (arr, id) {
-      for (var i = 0; i < arr.length; i++) {
-        if (arr[i].id === id) {
+    var addAmount = function (store, ep) {
+      for (var i = 0; i < store.length; i++) {
+        if (store[i].ep === ep) {
           return i
         }
       }
     }
-    // work
   })
